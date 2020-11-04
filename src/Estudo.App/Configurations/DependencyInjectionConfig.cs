8ce1b8119bce110988a112fdea4ac1f664961b00
Monique de Oliveira <oliveira29.monique@gmail.com>
2020-11-04 18:44:05 -0300

    * Melhorias no arquivo Startup e criação de TagHelper.

---
+++ src/Estudo.App/Configurations/DependencyInjectionConfig.cs
@@ -0,0 +1,23 @@
+﻿using Estudo.App.Extensions;
+using Estudo.Business.Interfaces;
+using Estudo.Data.Context;
+using Estudo.Data.Repository;
+using Microsoft.AspNetCore.Mvc.DataAnnotations;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Estudo.App.Configurations
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection ResolveDependencies(this IServiceCollection services)
+        {
+            services.AddTransient<MeuDbContext>();
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            services.AddScoped<IFornecedorRepository, FornecedorRepository>();
+            services.AddScoped<IEnderecoRepository, EnderecoRepository>();
+            services.AddSingleton<IValidationAttributeAdapterProvider, MoedaValidationAttributeAdapterProvider>();
+
+            return services;
+        }
+    }
+}
